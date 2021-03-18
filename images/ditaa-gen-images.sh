0d6212ec31fbb74155536be0921ad0c0d27767e5
Raif Ahmed <rahmed@redhat.com>
2021-03-18 16:42:22 +0200

    Update

--- images/ditaa-gen-images.sh
+++ images/ditaa-gen-images.sh
@@ -1,3 +1,4 @@
 java -jar ./ditaa-0.11.0-standalone.jar usecase-1.ditaa usecase-1.png
 java -jar ./ditaa-0.11.0-standalone.jar usecase-2.ditaa usecase-2.png
 java -jar ./ditaa-0.11.0-standalone.jar usecase-3.ditaa usecase-3.png
+java -jar ./ditaa-0.11.0-standalone.jar usecase-4.ditaa usecase-4.png
